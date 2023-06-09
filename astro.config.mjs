8b1666c8091e34e6bba59e6e38e46d71a6902fde
Xabi Trigo <xabitrigo@gmail.com>
2023-06-09 16:00:19 +0200

    Improved SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site: "https://unioncoruna.github.io",
-  // base: "/unioncoruna.github.io",
+  integrations: [tailwind(), sitemap()],
+  site: "https://unioncoruna.com", // base: "/unioncoruna.github.io",
 });
